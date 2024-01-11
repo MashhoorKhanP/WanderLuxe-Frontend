8d6a7e807fba8565fc6cb3ea0f003b9657252a7f
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-11 21:43:37 +0530

    axios.ts deployment errors fixed

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
   withCredentials: true,
 });
 
