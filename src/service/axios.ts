8e9fe538dd311417b28e6f4a4b9da277f52c38ac
Mashhoor Khan P <mashhoorkhan325pc@gmail.com>
2024-01-12 09:47:06 +0530

    axios.ts updated

--- src/service/axios.ts
+++ src/service/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}/api`,
+  baseURL: `${import.meta.env.VITE_SERVER_URL}`,
   withCredentials: true,
 });
 
